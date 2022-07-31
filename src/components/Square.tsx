a460fa79c06c7fe8d611f15dc66e13d99bc1cee6
Yuki Yamamura <yuki.yamamura@icloud.com>
2022-08-01 06:19:55 +0900

    chore: replace files in src directory.
    Add some new files after deleting all the files in src directory to set up local development environment.

---
+++ src/components/Square.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
