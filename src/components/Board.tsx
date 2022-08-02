528ee2c21e5be1507a41a7b18150c06cb9e27959
Yuki Yamamura <yuki.yamamura@icloud.com>
2022-08-02 10:32:46 +0900

    feat: add necessary logics for tic-tac-toe game

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,8 +1,55 @@
+import { useState, useEffect, useCallback } from 'react';
 import { Square } from './Square';
 
+export type SquareValue = 'X' | 'O' | null;
+
 export const Board = () => {
-  const renderSquare = (i: number) => <Square />;
-  const status = 'Next player: X';
+  const [squares, setSquares] = useState<SquareValue[]>(Array(9).fill(null))
+  const [xIsNext, setXISNext] = useState(true);
+  const [winner, setWinner] = useState<SquareValue>(null);
+
+  const status = winner !== null
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  type Squares = typeof squares;
+  const calculateWinner = useCallback((squares: Squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const filledLIne = lines.find((line) => {
+      const [a, b, c] = line;
+      return squares[a] !== null
+        && squares[a] === squares[b]
+        && squares[a] === squares[c];
+    });
+    return filledLIne !== undefined ?
+      squares[filledLIne[0]] : null;
+  }, []);
+
+  const handleClick = (i: number) => {
+    if (winner !== null || squares[i]) {
+      return;
+    }
+
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXISNext(!xIsNext);
+  }
+  const renderSquare = (i: number) =>
+    <Square value={squares[i]} onClick={() => handleClick(i)} />;
+
+  useEffect(() => {
+    setWinner(calculateWinner(squares));
+  }, [calculateWinner, squares])
 
   return (
     <div>
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,15 @@
-export const Square = () => (
-  <button className="square">
-    {/* TODO */}
+import React from "react";
+import { SquareValue } from './Board';
+
+type Props = {
+  value: SquareValue;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export const Square = ({ value, onClick }: Props) =>
+  <button
+    className='square'
+    onClick={onClick}
+  >
+    {value}
   </button>
-)
