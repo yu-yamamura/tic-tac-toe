f25921029f9b6f244410560f6f5ac2d93368e4c7
Yuki Yamamura <yuki.yamamura@icloud.com>
2022-08-28 04:50:23 +0900

    style: improve formats in `Game.tsx`

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,9 +4,9 @@ import { History } from '../types/History';
 import { Board } from './Board';
 
 export const Game = () => {
-  const [history, setHistory] = useState<History>(
-    [{ squares: Array(9).fill(null) as BoardSquares} ]
-  );
+  const [history, setHistory] = useState<History>([
+    { squares: Array(9).fill(null) as BoardSquares },
+  ]);
   const [xIsNext, setXISNext] = useState(true);
   const [stepNumber, setStepNumber] = useState(0);
 
@@ -23,13 +23,14 @@ export const Game = () => {
     ] as const;
     const lineCausedOfWin = lines.find((line) => {
       const [a, b, c] = line;
-      return squares[a] !== null
-        && squares[a] === squares[b]
-        && squares[a] === squares[c];
+      return (
+        squares[a] !== null &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      );
     });
 
-    return lineCausedOfWin !== undefined ?
-      squares[lineCausedOfWin[0]] : null;
+    return lineCausedOfWin !== undefined ? squares[lineCausedOfWin[0]] : null;
   }, []);
 
   const handleClick = (i: number) => {
@@ -40,19 +41,20 @@ export const Game = () => {
     // The second parameter of Array.prototype.slice() doesn't include its element, so add 1 to stepNumber.
     const newHistory = history.slice(0, stepNumber + 1);
 
-    setHistory(newHistory.concat([{ squares: newSquares }]))
+    setHistory(newHistory.concat([{ squares: newSquares }]));
     setXISNext(!xIsNext);
     setStepNumber(newHistory.length);
-  }
+  };
 
   const jumpTo = (stepNumber: number) => {
     setStepNumber(stepNumber);
-    setXISNext((stepNumber % 2) === 0);
-  }
+    setXISNext(stepNumber % 2 === 0);
+  };
 
   const current = history[stepNumber];
   const winner = useMemo(
-    () => calculateWinner(current.squares), [calculateWinner, current.squares]
+    () => calculateWinner(current.squares),
+    [calculateWinner, current.squares],
   );
 
   const getMoveLocation = (step: number) => {
@@ -79,17 +81,13 @@ export const Game = () => {
   return (
     <div className="game">
       <div className="game-board">
-        <Board
-          squares={current.squares}
-          handleClick={handleClick}
-        />
+        <Board squares={current.squares} handleClick={handleClick} />
       </div>
       <div className="game-info">
         <div>
           {winner !== null
             ? `Winner: ${winner}`
-            : `Next player: ${xIsNext ? 'X' : 'O'}`
-          }
+            : `Next player: ${xIsNext ? 'X' : 'O'}`}
         </div>
         <ol>
           {history.map((_, move) => (
@@ -103,5 +101,5 @@ export const Game = () => {
         </ol>
       </div>
     </div>
-  )
-}
+  );
+};
